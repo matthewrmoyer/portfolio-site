21bffb02626624a951462cef60fd1bc0af34fc55
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-08 15:52:00 -0400

    og commit

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('.about').addClass('display-none')
+	// $('.project-container').addClass('display-none')
+	$('.languages-and-tools-container').addClass('display-none')
+
+
+	$("body").on("click", ".list-item-text", function(e) {
+		//without this the click on the netflix-play-button would also click the module-movie
+		e.stopPropagation();
+		$('.navbar-list-item').removeClass('accent-blue-border')
+		$(this).parent('.navbar-list-item').addClass('accent-blue-border');
+		console.log(this)
+		var navItemText = this.innerText;
+		console.log(navItemText)
+
+		if (navItemText == "Projects") {
+			console.log('text is projects')
+			$('.project-container').removeClass('display-none')
+			$('.about').addClass('display-none')
+			$('.languages-and-tools-container').addClass('display-none')
+		} else if (this.innerText == "About") {
+			$('.about').removeClass('display-none')
+			$('.languages-and-tools-container').removeClass('display-none')
+			$('.project-container').addClass('display-none')
+		}
+	});
+
+});
